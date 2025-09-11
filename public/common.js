a319ea604f218bc5133b5f0090ff3204bb2e3882
smallgut <tobylau2023@gmail.com>
2025-09-12 06:26:55 +0800

    Update common.js

--- public/common.js
+++ public/common.js
@@ -3,7 +3,7 @@ let supabaseClient = null;
 async function ensureSupabaseClient() {
   if (!supabaseClient) {
     console.log("Initializing Supabase Client...");
-    supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+    supabaseClient = window.supabase.createClient(https://aouduygmcspiqauhrabx.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvdWR1eWdtY3NwaXFhdWhyYWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNTM5MzAsImV4cCI6MjA2MDgyOTkzMH0.s8WMvYdE9csSb1xb6jv84aiFBBU_LpDi1aserTQDg-k);
   }
   return supabaseClient;
 }
